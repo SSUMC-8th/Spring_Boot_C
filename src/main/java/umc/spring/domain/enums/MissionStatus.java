fba47d7f6d2c4a95062f2458fb19e806911edc66
dmori <chosehoon2@naver.com>
2025-05-01 16:19:24 +0900

    practice/#6

--- src/main/java/umc/spring/domain/enums/MissionStatus.java
+++
@@ -1,5 +0,0 @@
-package umc.spring.domain.enums;
-
-public enum MissionStatus {
-    AVAILABLE ,IN_PROGRESS, COMPLETED
-}
--- src/main/java/umc/spring/domain/enums/NotificationType.java
+++
@@ -1,5 +0,0 @@
-package umc.spring.domain.enums;
-
-public enum NotificationType {
-    EVENT, REVIEW, INQUIRY
-}
