8bedd124e3ff8273014ffd3d531aee7bfccb2e39
daeun088 <kde2329@naver.com>
2025-05-14 02:16:30 +0900

    practice/#8

---
+++ src/main/java/umc/spring/web/controller/MemberRestController.java
@@ -0,0 +1,27 @@
+package umc.spring.web.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import umc.spring.apiPayload.ApiResponse;
+import umc.spring.apiPayload.code.MemberRequestDTO;
+import umc.spring.apiPayload.code.MemberResponseDTO;
+import umc.spring.converter.MemberConverter;
+import umc.spring.domain.Member;
+import umc.spring.service.MemberService.MemberCommandService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/members")
+public class MemberRestController {
+    private final MemberCommandService memberCommandService;
+
+    @PostMapping("/")
+    public ApiResponse<MemberResponseDTO.JoinResultDTO> join(@RequestBody @Valid MemberRequestDTO.JoinDto request){
+        Member member = memberCommandService.joinMember(request);
+        return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    }
+}
